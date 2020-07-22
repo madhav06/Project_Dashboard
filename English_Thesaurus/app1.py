d32a06d72bf48b42ad87b507cca073155494689e
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-22 16:16:32 +0530

    taking into account bad words

--- English_Thesaurus/app1.py
+++ English_Thesaurus/app1.py
@@ -2,10 +2,12 @@ import json
 
 data = json.load(open("data.json"))
 
-def translate(w):
-    return data[w]
+def translate(w): # w: local var
+    if w in data:
+        return data[w]  
+    else:    # taking into account bad words
+        return "The word doesn't exist. Please double check it."
 
-#global variable
-word = input("Enter word: ")
+word = input("Enter word: ")    # word: global var
 
 print(translate(word))
