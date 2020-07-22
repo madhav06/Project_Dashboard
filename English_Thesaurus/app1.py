d84a7a76d012af779ec03d8488cef1c921007088
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-22 23:08:33 +0530

    updates in app1.py

--- English_Thesaurus/app1.py
+++ English_Thesaurus/app1.py
@@ -2,9 +2,12 @@ import json
 
 data = json.load(open("data.json"))
 
-def translate(w): # w: local var
+def translate(w):    # w: local var
+    w = w.lower() 
     if w in data:
         return data[w]  
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:    # taking into account bad words
         return "The word doesn't exist. Please double check it."
 
