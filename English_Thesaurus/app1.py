e6278620e670efcef168dbd106338f4996346a27
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-23 22:41:26 +0530

    updates

--- English_Thesaurus/app1.py
+++ English_Thesaurus/app1.py
@@ -6,7 +6,9 @@ data = json.load(open("data.json"))
 def translate(w):    # w: local var
     w = w.lower() 
     if w in data:
-        return data[w]  
+        return data[w] 
+    elif w.upper() in data:    # in case user enter words  like INDIA,NATO,  USA etc
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
