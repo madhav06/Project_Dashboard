3e71919cd9f4b73e6c6145d18fabb494faffe06a
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-23 22:35:47 +0530

    optimizing the final output

--- English_Thesaurus/app1.py
+++ English_Thesaurus/app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,10 +8,25 @@ def translate(w):    # w: local var
     if w in data:
         return data[w]  
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your query."
     else:    # taking into account bad words
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")    # word: global var
 
-print(translate(word))
+#print(translate(word))
+
+# printing output more friendly:
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
