4df2c811b31c8186658ada1c2f5c97b8514b794f
MADHAV NANDAN <mnandan06@gmail.com>
2020-07-30 21:32:50 +0530

    code section

---
+++ Website_In_Python/code/list.py
@@ -0,0 +1,15 @@
+# # Lists :
+# A list is created by placing all the elements or items inside a square brackets 
+
+my_list = [] # no item here, so empty list.
+
+list_int = [12, 25, 23, 51]
+# print(list_int)
+
+list_mixed = [23, "data", 4.5]
+
+# print(list_mixed)
+# Negative indexing
+print(list_mixed[-2])
+
+# thank you!!!
---
+++ Website_In_Python/code/variable.py
@@ -0,0 +1,19 @@
+# A variable is a something that stores data at some location called " memory ".
+
+# new_var = 5
+# print(new_var)
+
+# price = 500
+# print(price)
+
+price = 401
+print(price)
+
+price = "High Five"
+print("Price = ", price)
+
+# thank you!!!
+
+
+
+
